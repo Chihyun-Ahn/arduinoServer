675b7d37f9a485356c00fad33f0f770df7641cbc
Chihyun-Ahn <anchi15@samwooinc.net>
2021-04-01 10:21:18 +0900

    20210401-1 HC12 test added

---
+++ HC12_sender/HC12_sender.ino
@@ -0,0 +1,34 @@
+#include <SoftwareSerial.h>
+
+String ID = "0001";
+String code = "01";
+int
+String msg = "xxxx";
+/*
+  Communication protocol
+  01: powered on
+  02: Sensor touched (Feed is full)
+  03: alive
+ */
+
+SoftwareSerial HC12(2,3); // HC-12 TX Pin, HC-12 RX Pin
+
+void setup() {
+  Serial.begin(9600);
+  HC12.begin(9600);         // Serial port to HC12
+  String msg = String(ID + code);
+  HC12.println(msg);
+  Serial.println(msg);
+  delay(100);
+}
+
+void loop()
+{
+  code = "03";
+  msg = String(ID+code);
+  HC12.println(msg);
+  Serial.print("Msg sent: ");
+  Serial.print(msg);
+  Serial.print('\n');
+  delay(800);
+}
